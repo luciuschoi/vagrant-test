fe3f9892490e41d280a90e6dc7f52c90d59f2a48
Lucius Choi <lucius.choi@gmail.com>
2018-07-03 15:37:52 +0900

    fixed an error on attr function parameter

--- app/assets/javascripts/channels/comments.js
+++ app/assets/javascripts/channels/comments.js
@@ -3,7 +3,7 @@ App.comments = App.cable.subscriptions.create('CommentsChannel', {
     if (parent_id) {
       // console.log("parent_id defined...." + parent_id)
       $comment_parent = $("[data-channel='comment-" + parent_id + "'] > ul");
-      if(!$comment_parent.length){
+      if (!$comment_parent.length) {
         $("[data-channel='comment-" + parent_id + "']").append("<ul></ul>");
         $comment_parent = $("[data-channel='comment-" + parent_id + "'] > ul");
       }
@@ -28,7 +28,7 @@ App.comments = App.cable.subscriptions.create('CommentsChannel', {
       current_user_id = document.querySelector("meta[name='current-user']").getAttribute("data-id");
       console.info("*comment user id : " + comment_user_id);
       console.info("*current user id : " + current_user_id);
-      if (comment_user_id != current_user_id){
+      if (comment_user_id != current_user_id) {
         data.comment = data.comment.replace(/<span class='dot-bullet'><a class="(edit|delete)-comment-link"\s.*?>.*?<\/a><\/span>/g, '');
         console.info(data.comment);
         console.log(data.comment_action);
@@ -40,7 +40,7 @@ App.comments = App.cable.subscriptions.create('CommentsChannel', {
       if (data.comment_action == 'create') {
         console.log('created');
         $comments_count = $("#comments-count-of-commentable-" + data.commentable_id);
-        $comments_count.html(data.commentable_comments_size).effect('highlight',{}, 1000);
+        $comments_count.html(data.commentable_comments_size).effect('highlight', {}, 1000);
         return this.collection(data.parent_id).append(data.comment);
       };
       if (data.comment_action == 'update') {
@@ -51,14 +51,16 @@ App.comments = App.cable.subscriptions.create('CommentsChannel', {
     if (data.comment_action == 'destroy') {
       console.log('deleted');
       $comments_count = $("#comments-count-of-commentable-" + data.commentable_id);
-      $comments_count.html(data.commentable_comments_size).effect('highlight',{}, 1000);
-      return $("#comment_" + data.comment_id).effect("highlight", { color: '#f7937c' }, 500).slideUp('fast');
+      $comments_count.html(data.commentable_comments_size).effect('highlight', {}, 1000);
+      return $("#comment_" + data.comment_id).effect("highlight", {
+        color: '#f7937c'
+      }, 500).slideUp('fast');
     }
 
   },
 
   userIsCurrentUser: function (comment) {
-    return $(comment).attr('data-user-id') === $('meta[name=current-user]').attr('id');
+    return $(comment).attr('data-user-id') === $('meta[name=current-user]').attr('data-id');
   },
 
   disconnected: function () {},
